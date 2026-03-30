caa42fe45ee9155dbb62b06b375826d2061fe303
Ymani Williams <ywilliams0526@floridapoly.edu>
2026-03-30 17:47:17 +0000

    begin

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AgileActivity
+
+The .venv folder isn't used (but I'm not removing it since I don't know if that'll break anything). The reason why
+is because I used a virtual environment initially, but that didn't work out.
+
+By extension, I don't know if the ```*project.toml*``` file is required either, but it's there just in case.
--- app.py
+++ app.py
@@ -1,4 +1,32 @@
 import pandas as pd
 import streamlit as st
 
+# Handle the main interface
+
+st.header("Faculty Grade Dashboard")
+
+# Updates the dashboard on the main page
+def updateDashboard():
+    st.metric("Average", 0)
+    st.metric("Highest", 0)
+    st.metric("Lowest", 0)
+
 # Handle the UI Sidebar
+
+sd_bar = st.sidebar
+sd_bar.header("Entry Form")
+
+## Request for the student name
+
+student_name = sd_bar.text_input("Student Name", placeholder = "Lorem Ipsum")
+
+## Request for their grade
+
+student_grade = sd_bar.number_input("Score", step = 1)
+
+## Add the student to the list
+
+if sd_bar.button("Add Student"):
+    ## placeholder
+    st.text(f"Added Student {student_name}")
+    updateDashboard()
