a3ffa9dedfe0422854e9194ee5fa25de04fbee04
Ymani Williams <ywilliams0526@floridapoly.edu>
2026-03-30 18:13:56 +0000

    app.py update

--- app.py
+++ app.py
@@ -1,15 +1,57 @@
 import pandas as pd
 import streamlit as st
 
+# Main variables
+
+df = None
+testing = True
+
 # Handle the main interface
 
 st.header("Faculty Grade Dashboard")
 
+# Load data
+
+## Create the data for the session
+if 'data' not in st.session_state:
+    st.session_state.data = pd.DataFrame(columns=["Student", "Grade"])
+
 # Updates the dashboard on the main page
 def updateDashboard():
-    st.metric("Average", 0)
-    st.metric("Highest", 0)
-    st.metric("Lowest", 0)
+    data = None
+
+    # Test input data
+    if testing:
+        data = {
+            "avg": 0.4555,
+            "high": 89,
+            "low": 59
+        }
+
+    c1, c2, c3 = st.columns(3)
+    c1.metric("Average", data["avg"])
+    c2.metric("Highest", data["high"])
+    c3.metric("Lowest", data["low"])
+
+    st.subheader("Grade Distribution")
+    st.bar_chart(df)
+
+    st.subheader("Current Roster")
+    st.table(df)
+
+# Adds the student to the data entries (updates the dashboard)
+def addStudent(student_name, score):
+    new_entry = pd.DataFrame({"Student": [student_name], "Grade": [score]})
+    st.session_state.data = pd.concat([st.session_state.data, new_entry], ignore_index=True)
+    #df = st.session_state.data
+    updateDashboard()
+
+# Tell the user to begin inputing data
+if not st.session_state.data.empty:
+    df = st.session_state.data
+    updateDashboard()
+else:
+    st.info("No students inputed.")
 
 # Handle the UI Sidebar
 
@@ -28,5 +70,5 @@ student_grade = sd_bar.number_input("Score", step = 1)
 
 if sd_bar.button("Add Student"):
     ## placeholder
-    st.text(f"Added Student {student_name}")
-    updateDashboard()
+    ## st.text(f"Added Student {student_name}")
+    addStudent(student_name, student_grade)
